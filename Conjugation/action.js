b00b73361b7643ce30a1800031261ecce014e330
Captain-jack0 <meryem.dilara788@gmail.com>
2023-05-21 23:33:53 +0300

    pages added

---
+++ Conjugation/action.js
@@ -0,0 +1,9 @@
+
+// JavaScript to update the progress bar
+function updateProgressBar(percent) {
+  var progressBar = document.getElementById('progress');
+  progressBar.style.width = percent + '%';
+}
+
+// Example usage: updateProgressBar(50); to set progress to 50%
+
---
+++ Nivels/action.js
@@ -0,0 +1,11 @@
+window.addEventListener('popstate', function(event) {
+    if (event.state && event.state.reloaded === true) {
+      location.reload(); // Reload the page
+    }
+  });
+
+  function goToPage(page) {
+    history.pushState({ reloaded: true }, '','http://localhost/SpanishLearning/'+  page);
+    location.reload(); // Reload the page immediately
+  }
+
